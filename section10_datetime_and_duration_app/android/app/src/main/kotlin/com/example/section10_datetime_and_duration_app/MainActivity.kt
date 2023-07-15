476c1f8dcf743e58738776bf1077c275b0e1927c
hyunseokSon <fairy0724@soongsil.ac.kr>
2023-07-15 14:36:30 +0900

    D-Day 연인 앱(DatePicker, DateTime, Duration 이용)

---
+++ section10_datetime_and_duration_app/android/app/src/main/kotlin/com/example/section10_datetime_and_duration_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.section10_datetime_and_duration_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ section10_datetime_and_duration_app/lib/screen/home_screen.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // 100~900 까지 색 값읇 변경해줄수 있다. 500이 기본값이다.
+        backgroundColor: Colors.pink[100],
+        body: SafeArea(
+          bottom: false,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                _TopPart(),
+                _BottomPart(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+// private 을 위해 '_' 로 시작!
+// 윗 부분에 해당하는 UI 디자인 코드
+class _TopPart extends StatefulWidget {
+  const _TopPart({super.key});
+
+  @override
+  State<_TopPart> createState() => _TopPartState();
+}
+
+class _TopPartState extends State<_TopPart> {
+  // 기본값을 현재 날짜로 지정한다.
+  DateTime selectedDate = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 80.0,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                '우리 처음 만난 날',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'sunflower',
+                  fontSize: 30.0,
+                ),
+              ),
+              Text(
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'sunflower',
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+
+          IconButton(
+            iconSize: 60.0,
+            onPressed: () {
+              // dialog
+              showCupertinoDialog(
+                  context: context,
+                  // 흰색 Container 바깥 부분을 누르면 닫힌다.
+                  barrierDismissible: true,
+                  builder: (BuildContext context) {
+                    // dialog 작성
+                    return Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        color: Colors.white,
+                        height: 300.0,
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.date,
+                          // 초기 날짜 설정
+                          initialDateTime: selectedDate,
+
+                          // 최대 날짜 설정
+                          maximumDate: DateTime(
+                            now.year,
+                            now.month,
+                            now.day,
+                          ),
+                          onDateTimeChanged: (DateTime date){
+                            setState(() {
+                              // 선택한 날짜로 지정!
+                              selectedDate = date;
+                            });
+                          },
+                        ),
+                      ),
+                    );
+                  });
+            },
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+          ),
+          Text(
+            'D+${
+            DateTime(
+              now.year,
+              now.month,
+              now.day,
+            ).difference(selectedDate).inDays + 1
+            }',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 하위 UI 작성
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        'asset/img/middle_image.png',
+      ),
+    );
+  }
+}
