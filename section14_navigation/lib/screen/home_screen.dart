3cafa052319d9bc4ffee0472cffd3ac78041454b
hyunseokSon <fairy0724@soongsil.ac.kr>
2023-07-17 15:26:48 +0900

    WillPopScope : 시스템 상에서 뒤로가기 여부 설정

--- section14_navigation/lib/screen/home_screen.dart
+++ section14_navigation/lib/screen/home_screen.dart
@@ -7,44 +7,55 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).canPop();
-          },
-          child: Text('Can Pop'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).maybePop();
-          },
-          child: Text(
-            'Maybe Pop',
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Pop'),
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => RouteOneScreen(
-                  number: 123,
-                ),
-              ),
-            );
+    return WillPopScope(
+      onWillPop: () async {
+        // true - pop 가능
+        // false - pop 불가능
 
-            print(result);
-          },
-          child: Text('Push!'),
-        ),
-      ],
+        // 작업 따로 설정
+        final canPop = Navigator.of(context).canPop();
+
+        return canPop;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).canPop();
+            },
+            child: Text('Can Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).maybePop();
+            },
+            child: Text(
+              'Maybe Pop',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RouteOneScreen(
+                    number: 123,
+                  ),
+                ),
+              );
+
+              print(result);
+            },
+            child: Text('Push!'),
+          ),
+        ],
+      ),
     );
   }
 }
